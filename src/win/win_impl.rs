606b0308b198638e3871a00d0a5ada69fe24deba
Zeldern <31072838+Zeldern@users.noreply.github.com>
2020-03-04 21:30:15 +0000

    t

--- src/win/win_impl.rs
+++ src/win/win_impl.rs
@@ -47,12 +47,11 @@ fn keybd_event(flags: u32, vk: u16, scan: u16) {
 
 impl MouseControllable for Enigo {
     fn mouse_move_to(&mut self, x: i32, y: i32) {
-        let 
         mouse_event(
             MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE,
             0,
-            ((x - unsafe { GetSystemMetrics(76) }) * 65536 / unsafe { GetSystemMetrics(78) }),
-            ((x - unsafe { GetSystemMetrics(77) }) * 65536 / unsafe { GetSystemMetrics(79) }),
+            (x - unsafe { GetSystemMetrics(76) }) * 65536 / unsafe { GetSystemMetrics(78) },
+            (x - unsafe { GetSystemMetrics(77) }) * 65536 / unsafe { GetSystemMetrics(79) },
         );
     }
 
